fa651c54d35ced38a8e6a2e28e6790f720cc9183
cardenio@udel.edu <cardenio@udel.edu>
2018-01-21 22:23:20 +0000

    automatically backed up by learn

--- variables.js
+++ variables.js
@@ -5,8 +5,10 @@ var personality = "awesome"
 console.log(personality)
 //
 
-personality = "fabulous";
+personality = "sassy";
 console.log(personality);
+
+var totalSum = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000
 
 // Your Turn!
 // change the value of the `personality` variable to something that describes you
